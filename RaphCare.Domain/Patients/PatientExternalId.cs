abcc7837c28b8abd0073e5f5776863fed073508b
loedask <loedask@hotmail.com>
2026-03-16 23:48:52 +0200

    Refactor PatientExternalId class to inherit from BaseEntity, enhancing consistency with other domain entities. Add necessary using directive for RaphCare.Domain.Common.

--- RaphCare.Domain/Patients/PatientExternalId.cs
+++ RaphCare.Domain/Patients/PatientExternalId.cs
@@ -1,9 +1,11 @@
+using RaphCare.Domain.Common;
+
 namespace RaphCare.Domain.Patients;
 
 /// <summary>
 /// External system identifier for a global patient record (e.g., clinic, insurer, lab, national registry).
 /// </summary>
-public class PatientExternalId : Entity
+public class PatientExternalId : BaseEntity
 {
     public Guid PatientId { get; set; }
 
